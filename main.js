0968e347d74f831881fa748db1948e40d6bdd755
kyunggu94 <kyunggu94@gmail.com>
2026-05-02 16:23:56 +0000

    Update project files and configure repository

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-// Add JS here
+const lottoNumbersContainer = document.querySelector('.lotto-numbers');
+const generateBtn = document.getElementById('generate-btn');
+
+function generateLottoNumbers() {
+    lottoNumbersContainer.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+
+    const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
+
+    for (const number of sortedNumbers) {
+        const numberDiv = document.createElement('div');
+        numberDiv.classList.add('number');
+        numberDiv.textContent = number;
+        lottoNumbersContainer.appendChild(numberDiv);
+    }
+}
+
+generateBtn.addEventListener('click', generateLottoNumbers);
